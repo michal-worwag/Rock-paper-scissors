c09b0591730cc29183d4caa383fe683db515532e
Michał Worwąg <michalwo@gmail.com>
2018-07-17 14:19:10 +0200

    Add classes for modal and js for modals

--- js/index.js
+++ js/index.js
@@ -107,3 +107,44 @@ function newGame() {
   }};
 
 newGameBtn.addEventListener("click", newGame);
+
+// modale
+
+var showModal = function (event){
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.add('show');
+  var removeShow = document.querySelectorAll('.modal');
+  for (var i = 0; i < removeShow.length; i++){
+    removeShow[i].classList.remove('show');
+  }
+  var modal = document.querySelector(event.target.getAttribute("href"));
+  modal.classList.add('show'); 
+ };
+
+var modalLinks = document.querySelectorAll('.show-modal');
+
+for(var i = 0; i < modalLinks.length; i++){
+  modalLinks[i].addEventListener('click', showModal);
+}
+
+var hideModal = function(event){
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.remove('show');
+};
+
+var closeButtons = document.querySelectorAll('.modal .close');
+  for(var i = 0; i < closeButtons.length; i++){
+  closeButtons[i].addEventListener('click', hideModal);
+}
+
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+
+var modals = document.querySelectorAll('.modal');
+
+for(var i = 0; i < modals.length; i++){
+  modals[i].addEventListener('click', function(event){
+    event.stopPropagation();
+  });
+}	
+
+// koniec modali
