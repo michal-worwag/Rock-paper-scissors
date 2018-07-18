53be2d230446bf93a80721c8cc0a4303866e1b7d
Michał Worwąg <michalwo@gmail.com>
2018-07-18 13:03:02 +0200

    Almost finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var paperButton = document.getElementById("paperButton");
 var rockButton = document.getElementById("rockButton");
 var scissorsButton = document.getElementById("scissorsButton");
@@ -8,16 +10,16 @@ var computerPoints = document.getElementById("computerPoints");
 var newGameBtn = document.getElementById("newGameBtn");
 var roundsSpace = document.getElementById("roundsSpace");
 var errorRound = document.getElementById("errorRound");
-
+var tbody = document.querySelector('#tbody');
 var playerMoveBtn = document.querySelectorAll('.player-move');
 
 var params = {
-  rounds: 0,
+  winner: 0,
   wins: 0,
   lost: 0,
-  roundsPlayed: 0
-}
-
+  roundsPlayed: 0,
+  progress: []
+};
 
 for (var i = 0; i < playerMoveBtn.length; i++){
   playerMoveBtn[i].addEventListener('click', function(){
@@ -29,11 +31,18 @@ for (var i = 0; i < playerMoveBtn.length; i++){
 function playerMove(playerMove) {
   var computerMove = compMove();
   whoWin(playerMove, computerMove);
-}
+  
+  params.progress.push({
+    gameRounds: params.roundsPlayed,
+    gamePlayerMove: playerMove,
+    gameComputerMove: computerMove,
+    roundWinner: params.winner,
+    finalResult: params.wins + ' - ' + params.lost
+})
+};
 
 function compMove() {
   var compChoice = Math.floor(Math.random() * 3 + 1);
-  
   if (compChoice == 1) {
      return "PAPER"
   } else if (compChoice == 2) {
@@ -41,11 +50,13 @@ function compMove() {
   } else if (compChoice == 3) {
      return "SCISSORS"
   }
-}
+};
 
 function whoWin(playerMove, computerMove) {
   if (playerMove === computerMove) {
     output.innerHTML = "It's a TIE " + "You pick " +  playerMove + " Computer pick " + computerMove;
+    params.roundsPlayed++;
+    params.winner = "TIE";
   } else if (
     (playerMove === "ROCK" && computerMove === "PAPER") ||
     (playerMove === "SCISSORS" && computerMove === "ROCK") ||
@@ -53,12 +64,17 @@ function whoWin(playerMove, computerMove) {
   ) {
     output.innerHTML = "Computer WON " + "You pick " + playerMove + " Computer pick " + computerMove;
     params.lost++;
+    params.roundsPlayed++;
+    params.winner = "COMP";
   } else {
     output.innerHTML = "YOU WON: " + "You pick " + playerMove + " Computer pick " + computerMove;
     params.wins++;
+    params.roundsPlayed++;
+    params.winner = "PLAYER";
   }
   pointsCounter();
   gameFinish();
+
 };
 
 function pointsCounter() {
@@ -78,14 +94,15 @@ function gameFinish() {
     showModal()
     //output.innerHTML = "COMPUTER WON THE ENTIRE GAME!!!";
     buttonProp();
-  }};
+  }
+};
 
   function buttonProp(){
     newGameBtn.disabled = false;
     paperButton.disabled = true;
     rockButton.disabled = true;
     scissorsButton.disabled = true;
-  }
+  };
 
 function newGame() {
   params.rounds = prompt("How many rounds would You like to play?", 3);
@@ -104,7 +121,8 @@ function newGame() {
     scissorsButton.disabled = false;
     params.wins = params.lost = 0;
     pointsCounter();
-  }};
+  }
+};
 
 newGameBtn.addEventListener("click", newGame);
 
@@ -114,6 +132,7 @@ function showModal (){
   document.querySelector('.overlay').classList.add('show');
   var modal = document.querySelector('.modal');
   modal.classList.add('show'); 
+  buildTable();
  };
 
 var hideModal = function(event){
@@ -124,7 +143,7 @@ var hideModal = function(event){
 var closeButtons = document.querySelectorAll('.modal .close');
   for(var i = 0; i < closeButtons.length; i++){
   closeButtons[i].addEventListener('click', hideModal);
-}
+};
 
 document.querySelector('.overlay').addEventListener('click', hideModal);
 
@@ -134,6 +153,30 @@ for(var i = 0; i < modals.length; i++){
   modals[i].addEventListener('click', function(event){
     event.stopPropagation();
   });
-}	
+};
 
 // koniec modali
+
+// Tabela
+
+function buildTable(){
+    
+    params.progress.forEach(tableBuild)
+};
+
+function tableBuild(progressResult){
+    var row = document.createElement('tr');
+    tbody.appendChild(row);
+    for (var key in progressResult){
+        buildTableTd(progressResult[key], row);
+    }
+}
+
+function buildTableTd(value, row){
+    var td = document.createElement('td');
+    td.innerHTML = value;
+    row.appendChild(td);
+};
+
+
+
